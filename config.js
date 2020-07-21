a1d43f421d4b7641e52f0e673b6b513fd4fda5f8
David Ranich <ranich.david@gmail.com>
2020-07-21 19:58:37 -0400

    added new tenant key, and updated package.json

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-"db": {
-    url: 'mongodb+srv://david:danich93@cluster0-zexcr.gcp.mongodb.net/test_dispensary?retryWrites=true&w=majority',
-    options: { useNewUrlParser: true, useUnifiedTopology: true },
-    tenantKey: 'abc'
+    "db": {
+        url: 'mongodb+srv://davidranich:danich93@cluster0.zyvdf.mongodb.net/weed-suite?retryWrites=true&w=majority',
+        options: { useNewUrlParser: true, useUnifiedTopology: true },
+        tenantSecret: 'weeddaddythejackalzaps'
     }
 };
