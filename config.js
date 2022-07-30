4a3a8ec703058f9e6296ab653eb02792598efe40
David Ranich <ranich.david@gmail.com>
2022-07-30 00:46:21 -0400

    for whatever reason left credentials in here; project is dead tho

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 "db": {
-    url: 'mongodb+srv://david:danich93@cluster0-zexcr.gcp.mongodb.net/test_dispensary?retryWrites=true&w=majority',
+    url: 'empty',
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     tenantKey: 'eyJhbGciOiJIUzI1NiJ9.VGVzdCBEaXNwZW5zYXJ5ICMy.6ZGSPUUg6d0pkulQunDPt7jCUuClm8ma3N6Nzpo91Ww',
     tenantSecret: 'weeddaddyzachmetzvalenti'
